0bc0ef85ef771ec8e0aaa29bd8c28ebecb1c9d15
Łukasz Tenerowicz <konkit@gmail.com>
2024-12-05 19:58:35 +0100

    Day five - finished

--- dayfive/dayfive.go
+++ dayfive/dayfive.go
@@ -14,8 +14,100 @@ func Run() {
 		log.Fatal(err)
 	}
 
-	res := Rssult1(rules, updates)
+	res := Rssult2(rules, updates)
 	fmt.Println(res)
+}
+
+func Rssult1(rules []Rule, updates [][]int) int {
+	sum := 0
+	for _, update := range updates {
+		correctUpdate := checkUpdate(rules, update)
+
+		if correctUpdate {
+			sum += getMiddleElement(update)
+		}
+	}
+
+	return sum
+}
+
+func Rssult2(rules []Rule, updates [][]int) int {
+	sum := 0
+	for _, update := range updates {
+		isCorrect := checkUpdate(rules, update)
+
+		if isCorrect {
+			//sum += getMiddleElement(update)
+		} else {
+			makeUpdateCorrect(update, rules)
+			sum += getMiddleElement(update)
+		}
+	}
+
+	return sum
+}
+
+func makeUpdateCorrect(update []int, rules []Rule) {
+	changed := true
+	for changed == true {
+		changed = false
+
+		for _, rule := range rules {
+			aPos := findElement(update, rule.A)
+			if aPos == -1 {
+				continue
+			}
+
+			bPos := findElement(update, rule.B)
+			if bPos == -1 {
+				continue
+			}
+
+			if aPos > bPos {
+				swap(update, aPos, bPos)
+				changed = true
+			}
+		}
+	}
+}
+
+func swap(arr []int, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
+func findElement(update []int, val int) int {
+	for i := 0; i < len(update); i++ {
+		if update[i] == val {
+			return i
+		}
+	}
+	return -1
+}
+
+func checkUpdate(rules []Rule, update []int) bool {
+	entryToPosition := map[int]int{}
+
+	for i := 0; i < len(update); i++ {
+		entryToPosition[update[i]] = i
+	}
+
+	for _, rule := range rules {
+		aPos, ok := entryToPosition[rule.A]
+		if !ok {
+			continue
+		}
+
+		bPos, ok := entryToPosition[rule.B]
+		if !ok {
+			continue
+		}
+
+		if aPos > bPos {
+			return false
+		}
+	}
+
+	return true
 }
 
 func readInput() ([]Rule, [][]int, error) {
@@ -64,118 +156,6 @@ type Rule struct {
 	B int
 }
 
-type Graph struct {
-	adjList  map[int][]int
-	inDegree map[int]int
-}
-
-func NewGraph() *Graph {
-	g := &Graph{
-		adjList:  make(map[int][]int),
-		inDegree: make(map[int]int),
-	}
-	//for i := 0; i < n; i++ {
-	//	g.adjList[i] = []int{}
-	//	g.inDegree[i] = 0
-	//}
-	return g
-}
-
-func (g *Graph) AddEdge(rule Rule) {
-	g.adjList[rule.A] = append(g.adjList[rule.A], rule.B)
-
-	val, ok := g.inDegree[rule.A]
-	if !ok {
-		g.inDegree[rule.A] = 0
-	}
-
-	val, ok = g.inDegree[rule.B]
-	if ok {
-		g.inDegree[rule.B] = val + 1
-	} else {
-		g.inDegree[rule.B] = 1
-	}
-}
-
-func (g *Graph) TopologicalSort() []int {
-	queue := []int{}
-	result := []int{}
-
-	// Find all nodes with in-degree 0
-	for node, degree := range g.inDegree {
-		if degree == 0 {
-			queue = append(queue, node)
-		}
-	}
-
-	for len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
-		result = append(result, node)
-
-		for _, neighbor := range g.adjList[node] {
-			g.inDegree[neighbor]--
-			if g.inDegree[neighbor] == 0 {
-				queue = append(queue, neighbor)
-			}
-		}
-	}
-
-	// If there's a cycle, the graph is not a DAG
-	for _, degree := range g.inDegree {
-		if degree > 0 {
-			return nil
-		}
-	}
-
-	return result
-}
-
-func Rssult1(rules []Rule, updates [][]int) int {
-	g := NewGraph()
-
-	for _, rule := range rules {
-		g.AddEdge(rule)
-	}
-
-	result := g.TopologicalSort()
-
-	sum := 0
-
-	for _, update := range updates {
-		correctUpdate := true
-		cntVal := getIdOfNode(result, update[0])
-
-		for i := 1; i < len(update) && correctUpdate; i++ {
-			x := getIdOfNode(result, update[i])
-			if x < cntVal {
-				correctUpdate = false
-				continue
-			}
-			cntVal = x
-		}
-
-		if correctUpdate {
-			sum += getMiddleElement(update)
-		}
-	}
-
-	fmt.Println(result)
-	fmt.Println(sum)
-
-	return sum
-}
-
 func getMiddleElement(update []int) int {
 	return update[len(update)/2]
-}
-
-func getIdOfNode(result []int, node int) int {
-	for i := 0; i < len(result); i++ {
-		if result[i] == node {
-			return i
-		}
-	}
-
-	return -1
 }
--- dayfive/dayfive_test.go
+++ dayfive/dayfive_test.go
@@ -5,43 +5,53 @@ import (
 	"testing"
 )
 
-func TestRssult1(t *testing.T) {
-	rules := []dayfive.Rule{
-		{A: 47, B: 53},
-		{A: 97, B: 13},
-		{A: 97, B: 61},
-		{A: 97, B: 47},
-		{A: 75, B: 29},
-		{A: 61, B: 13},
-		{A: 75, B: 53},
-		{A: 29, B: 13},
-		{A: 97, B: 29},
-		{A: 53, B: 29},
-		{A: 61, B: 53},
-		{A: 97, B: 53},
-		{A: 61, B: 29},
-		{A: 47, B: 13},
-		{A: 75, B: 47},
-		{A: 97, B: 75},
-		{A: 47, B: 61},
-		{A: 75, B: 61},
-		{A: 47, B: 29},
-		{A: 75, B: 13},
-		{A: 53, B: 13},
-	}
+var rules = []dayfive.Rule{
+	{A: 47, B: 53},
+	{A: 97, B: 13},
+	{A: 97, B: 61},
+	{A: 97, B: 47},
+	{A: 75, B: 29},
+	{A: 61, B: 13},
+	{A: 75, B: 53},
+	{A: 29, B: 13},
+	{A: 97, B: 29},
+	{A: 53, B: 29},
+	{A: 61, B: 53},
+	{A: 97, B: 53},
+	{A: 61, B: 29},
+	{A: 47, B: 13},
+	{A: 75, B: 47},
+	{A: 97, B: 75},
+	{A: 47, B: 61},
+	{A: 75, B: 61},
+	{A: 47, B: 29},
+	{A: 75, B: 13},
+	{A: 53, B: 13},
+}
 
-	updates := [][]int{
-		[]int{75, 47, 61, 53, 29},
-		[]int{97, 61, 53, 29, 13},
-		[]int{75, 29, 13},
-		[]int{75, 97, 47, 61, 53},
-		[]int{61, 13, 29},
-		[]int{97, 13, 75, 29, 47},
-	}
+var updates = [][]int{
+	[]int{75, 47, 61, 53, 29},
+	[]int{97, 61, 53, 29, 13},
+	[]int{75, 29, 13},
+	[]int{75, 97, 47, 61, 53},
+	[]int{61, 13, 29},
+	[]int{97, 13, 75, 29, 47},
+}
+
+func TestRssult1(t *testing.T) {
 
 	result := dayfive.Rssult1(rules, updates)
 
 	if result != 143 {
+		t.Errorf("Expected 143, got %d", result)
+	}
+}
+
+func TestRssult2(t *testing.T) {
+
+	result := dayfive.Rssult2(rules, updates)
+
+	if result != 123 {
 		t.Errorf("Expected 143, got %d", result)
 	}
 }
